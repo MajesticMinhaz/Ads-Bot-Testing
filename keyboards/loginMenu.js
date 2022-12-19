53750cade97af325fe4ec8074e5175a0bc8f0e71
Md. Minhaz <mdm047767@gmail.com>
2022-12-19 18:49:09 +0600

    removed multilines or keyborad

--- keyboards/loginMenu.js
+++ keyboards/loginMenu.js
@@ -1,13 +1,9 @@
 const loginMenu = [
     [
         { text: '🗝️ Login', callback_data: 'login' },
-    ],
-    [
-        { text: '⚙️ Settings', callback_data: 'settings' }
-    ],
-    [
+        { text: '⚙️ Settings', callback_data: 'settings' },
         { text: '🦸‍♂️ Help', callback_data: 'help' }
-    ]
+    ],
 ];
 
 module.exports = {loginMenu};
