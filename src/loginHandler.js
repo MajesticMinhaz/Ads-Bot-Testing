772e4518fc27c2471dae8cac9344200670377f65
Md. Minhaz <mdm047767@gmail.com>
2022-12-19 11:15:01 +0600

    removed login handler from group

--- src/loginHandler.js
+++ src/loginHandler.js
@@ -7,16 +7,17 @@ const loginHandler = async (ctx) => {
 
     if (type === 'private') {
         await ctx.scene.enter('loginStage');
-    } else {
-        ctx.deleteMessage();
+    } 
+    // else {
+    //     ctx.deleteMessage();
 
-        ctx.replyWithPhoto(
-            "https://images.unsplash.com/photo-1506702315536-dd8b83e2dcf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHdyb25nJTIwcm9hZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-            {
-                caption: `Hey ${targetName}\n\n\nYou have to login using @${ctx.update.callback_query.message.from.username} this bot.\n\n`,
-            }
-        )
-    }
+    //     ctx.replyWithPhoto(
+    //         "https://images.unsplash.com/photo-1506702315536-dd8b83e2dcf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHdyb25nJTIwcm9hZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    //         {
+    //             caption: `Hey ${targetName}\n\n\nYou have to login using @${ctx.update.callback_query.message.from.username} this bot.\n\n`,
+    //         }
+    //     )
+    // }
 };
 
 module.exports = {loginHandler};
