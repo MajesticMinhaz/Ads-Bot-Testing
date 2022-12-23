7ba70d7e82c85e69e18816b3658a3f25fbcacad2
Md. Minhaz <mdm047767@gmail.com>
2022-12-23 19:14:10 +0600

    updated index file

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const { privateTypeHandler } = require('./middleware/privateTypeHandler');
 const { groupLeftTypeHandler, groupJoinTypeHandler } = require('./middleware/groupTypeHandler');
 const { channelTypeHandler } = require('./middleware/channelTypeHandler');
 const { updateLog } = require("./middleware/updateLog");
+const { backToMainMenuHandler } = require("./middleware/backToMainMenuHandler");
 const { startHandler } = require('./src/startHandler');
 const { loginHandler } = require('./src/loginHandler');
 const { logoutHandler } = require('./src/logoutHandler');
 const { channelsHandler } = require('./src/channelsHandler');
+const { dynamicButtonHandler } = require('./src/dynamicButtonHandler');
 const GetUserPasswordScene = require('./scenes/PasswordInput.scene');
 
 
@@ -29,14 +31,16 @@ bot.use(session());
 
 bot.use(PasswordInputStage.middleware());
 
-
 bot.start(startHandler);
-
+bot.help(errorMessageHandler);
 
 bot.action('login', loginHandler);
 bot.action('logout', logoutHandler);
 bot.action('channels', channelsHandler);
+bot.action('backToMainMenu', backToMainMenuHandler);
 bot.action('help', errorMessageHandler);
-bot.help(errorMessageHandler);
+
+bot.on('callback_query', dynamicButtonHandler);
 bot.on(['text', 'sticker', 'photo', 'video', 'document', 'poll', 'music', 'location', 'contact'], errorMessageHandler);
+
 bot.launch();
